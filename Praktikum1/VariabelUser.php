836f22fc3c62d482ee4d74cd2c7cb8b4fb038475
najmi563 <93016513+najmi563@users.noreply.github.com>
2022-03-06 19:50:00 +0700

    Add files via upload

---
+++ Praktikum1/VariabelUser.php
@@ -0,0 +1,15 @@
+<?php
+    // definisika variabels
+    $nama = "Najmi Taqiy Syach" ;
+    $umur = 19;
+    $berat = 65;
+
+    echo "Nama : " . $nama;
+    echo "<br/> Umur : " . $umur . " Tahun";
+    echo "<br/> Berat Badan : " . $berat . " kg";
+
+    echo "<br/>hallo $nama apakabar ?";
+
+
+
+?>
